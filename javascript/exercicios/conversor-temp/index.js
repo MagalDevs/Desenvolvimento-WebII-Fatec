3c042911b3c1a9daabd4cdef6d3a649e51dfb26c
DANIEL GALVAO MAGALHAES DA SILVA <1051392411001@fatec>
2024-08-26 10:58:15 -0300

    conversor feito 2.0

--- javascript/exercicios/conversor-temp/index.js
+++ javascript/exercicios/conversor-temp/index.js
@@ -1,10 +1,19 @@
-import PromptSync from "prompt-sync"
+import PromptSync from "prompt-sync";
 const prompt = PromptSync();
 
-function conversorTemp(){
-    let celsius = prompt("Digite uma temperatura em Celsius para conversão em Fahrenheit: ");
-    const Fahrenheit = (celsius * (9/5)) + 32;
-    console.log(`${celsius} ºC para Fahrenheit é equivalente à: ${Math.round(Fahrenheit)} ºF`)
+function conversorTemp() {
+    while(true){
+        let celsius = parseFloat(prompt("Digite uma temperatura em Celsius para conversão em Fahrenheit: "));
+        const Fahrenheit = (celsius * (9 / 5)) + 32;
+        
+        if (isNaN(celsius)) {
+            console.log("Por favor, insira um número válido.");
+            continue;
+        } else {
+            console.log(`${celsius} ºC para Fahrenheit é equivalente à: ${Math.round(Fahrenheit)} ºF`);       
+            break;
+        }
+    }
 }
 
 conversorTemp();
